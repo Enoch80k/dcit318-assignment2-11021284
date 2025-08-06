6f004f065c75c33563752e11a70507ec0153008c
Enoch80k <eskwarteng003@st.ug.edu.gh>
2025-08-06 02:12:58 +0000

    Add files via upload

---
+++ Abstract Classes and Methods/Abstract Classes and Methods/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+abstract class Shape
+{
+    
+    public abstract double GetArea();
+}
+
+class Circle : Shape
+{
+    private double radius;
+
+    
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    
+    public override double GetArea()
+    {
+        return Math.PI * radius * radius; 
+    }
+}
+
+class Rectangle : Shape
+{
+    private double width;
+    private double height;
+
+    
+    public Rectangle(double width, double height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+
+    
+    public override double GetArea()
+    {
+        return width * height; 
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        Shape circle = new Circle(4.0);
+        Shape rectangle = new Rectangle(4.0, 5.0); 
+
+       
+        Console.WriteLine($"Area of Circle: {circle.GetArea()}");
+        Console.WriteLine($"Area of Rectangle: {rectangle.GetArea()}");
+    }
+}
